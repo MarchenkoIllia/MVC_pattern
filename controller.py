95c9b70c387c3a8e5a6b05f7dd08e20c67028258
Illia Marchenko <lxgiwylaezakmi298@gmail.com>
2023-01-04 15:51:54 +0200

    check_scores added, controller.py - added defs

--- controller.py
+++ controller.py
@@ -2,4 +2,18 @@ import model
 import view as v
 
 def controllerLoop():
-    pass
+    user_choise = v.get_choise()
+    user_scores = 10
+    if user_choise == 1:
+        value = model.get_random_value()
+        score = model.get_scores(user_scores, value)
+        message = model.check_scores(score)
+        v.view_message(message)
+
+def main():
+    v.view_message('Сыграем в кости? (1): ')
+    f = v.get_loop()
+    f()
+
+if __name__ == '__main__':
+    main()
